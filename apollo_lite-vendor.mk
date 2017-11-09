78fcfd4347eeaa1d20ac21bb78d3e194a0a90aea
Noel Mohring <blackmantamen@gmail.com>
2017-11-09 20:44:58 +0100

    Init

---
+++ apollo_lite-vendor.mk
@@ -0,0 +1,13 @@
+# Live Wallpaper Packages
+PRODUCT_PACKAGES := \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni
+
+# Publish that we support the live wallpaper feature
+PRODUCT_COPY_FILES := \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+$(call inherit-product, vendor/vernee/apollo_lite/apollo_lite-vendor-blobs.mk)
