1d0263b7c477d35a202266d9398c969e960e19ec
Noel Mohring <blackmantamen@gmail.com>
2018-02-24 00:10:52 +0100

    Adding DRM blobs

--- apollo_lite-vendor.mk
+++ apollo_lite-vendor.mk
@@ -7,7 +7,6 @@ PRODUCT_PACKAGES := \
     librs_jni
 
 # Publish that we support the live wallpaper feature
-PRODUCT_COPY_FILES := \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+PRODUCT_COPY_FILES := packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 $(call inherit-product, vendor/vernee/apollo_lite/apollo_lite-vendor-blobs.mk)
